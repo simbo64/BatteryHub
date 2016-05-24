6c4da9135aeb805ff5227b9ea705bb4ef2c7efdc
Simon Edwards <simon.nomis@hotmail.co.uk>
2016-05-24 18:18:33 +0100

    Initial Commit

---
+++ Battery Hub/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Battery Hub
+//
+//  Created by Simon Edwardes on 24/05/2016.
+//  Copyright © 2016 Simon Edwardes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Battery Hub/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Battery Hub
+//
+//  Created by Simon Edwardes on 24/05/2016.
+//  Copyright © 2016 Simon Edwardes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
